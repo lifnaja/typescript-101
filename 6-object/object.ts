a59a8942163be8cc95b7094eab114686e1c55bb1
pops <pops@jitta.com>
2026-02-13 22:56:49 +0700

    feat: Add js demo path2

---
+++ 6-object/object.ts
@@ -0,0 +1,10 @@
+const dog = {
+    dogName: "JavaScript",
+    weight: 2.4,
+    color: "brown",
+    breed: "chihuahua",
+    age: 3,
+    burglarBiter: true
+}
+
+console.log(dog)
---
+++ 8-array-object/array-object.ts
@@ -0,0 +1,36 @@
+const companyActivities = {
+    companyName: "Healthy Candy",
+    activities: [
+        "food manufacturing",
+        "improving kids' health",
+        "manufacturing toys"
+    ],
+    address: {
+        street: "2nd street",
+        number: "123",
+        zipcode: "33116",
+        city: "Miami",
+        state: "Florida"
+    },
+    yearOfEstablishment: 2021
+}
+
+console.log(companyActivities)
+
+
+const addresses = [{
+    street: "2nd street",
+    number: "123",
+    zipcode: "33116",
+    city: "Miami",
+    state: "Florida"
+},
+{
+    street: "1st West avenue",
+    number: "5",
+    zipcode: "75001",
+    city: "Addison",
+    state: "Texas"
+}];
+
+console.log(addresses)
