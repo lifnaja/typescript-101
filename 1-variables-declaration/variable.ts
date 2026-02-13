5886420a80d5f90f8f30e08a356253ba83c1f2a2
Alif Ruksaithong <alif@odds.team>
2026-02-13 17:12:54 +0700

    Add example Variables Declaration

---
+++ 1-variables-declaration/variable.ts
@@ -0,0 +1,7 @@
+let nameStudent: string = "John Doe";
+let age: number = 30;
+let isStudent: boolean = false;
+
+console.log("Name: " + nameStudent);
+console.log("Age: " + age);
+console.log("Is Student: " + isStudent);
